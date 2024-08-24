a8a8ac970e3ea8acee7689bcb4ab462b2129cbdb
Edison Yang <edison.cy.yang@gmail.com>
2024-08-24 13:58:54 -0700

    pagination

--- src/routes/pokemons.ts
+++ src/routes/pokemons.ts
@@ -7,11 +7,25 @@ const router = Router()
 
 router.get('/pokemons', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const response = await axios.get(`${POKEAPI_BASE_URL}/pokemon?limit=100`)
+    const limit = parseInt(req.query.limit as string) || 50
+    const offset = parseInt(req.query.offset as string) || 0
+
+    if (limit < 1 || offset < 0) {
+      return res.status(400).json({ error: "Limit must be greater than 0 and offset must be non-negative"})
+    }
+
+    const response = await axios.get(`${POKEAPI_BASE_URL}/pokemon`, {
+      params: { limit, offset }
+    })
 
     const pokemonsList = response.data.results
 
-    res.json(pokemonsList)
+    res.json({
+      count: response.data.count,
+      next: response.data.next,
+      previous: response.data.previous,
+      results: pokemonsList,
+    })
   } catch (error) {
     next(error)
   }
